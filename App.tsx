dc474a01c0ea604b4c9d1a48b38d52b5f997af34
Bruno <brunohep86@gmail.com>
2025-06-20 16:46:37 +0200

    feat: inicio de alguns componentes da aplicacao

--- App.tsx
+++ App.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import Header from "./src/components/Header";
-import { View, Text} from 'react-native'
+import { View,} from 'react-native'
 import Post from './src/components/Post'
 
 
 function App() {
+  const comments = [{
+    nickname: 'Joana Elena Silva',
+    comment: 'Excelente Foto!'
+  },{
+    nickname: 'Rafael Gustavo Pereira',
+    comment: 'Muito ruim! Faco melhor'
+  }]
+
+
   return (
     <View style ={{flex: 1}}>
       <Header />
-      <Post image ={require('./assets/imgs/fence.jpg')} />
+      <Post image ={require('./assets/imgs/fence.jpg')} 
+        comments={comments}/>
     </View>
   );
 }
