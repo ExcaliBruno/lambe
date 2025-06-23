1c82815dbccbbc01f7de7c8446077523056bd9d9
Bruno <brunohep86@gmail.com>
2025-06-23 19:10:11 +0200

    feat: bottom navbar

---
+++ src/Navigator.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+import Feed from "./screens/Feed";
+
+const Tab = createBottomTabNavigator()
+
+export default function Navigator(){
+    return (
+        <NavigationContainer>
+            <Tab.Navigator 
+                screenOptions={({route})=> ({
+                    headerShown: false,
+                    tabBarShowLabel: false,
+                    tabBarIcon: ({ focused, color, size}) => {
+                        let iconName;
+
+                        if( route.name === 'Home') {
+                            iconName = 'home'
+                        } else if (route.name === 'AddPhoto'){
+                            iconName = 'camera'
+                        } else if (route.name === 'Profile') {
+                            iconName = 'user'
+                        }
+
+                        return (
+                            <Icon 
+                                name = {iconName}
+                                size = {focused? 30 : 25}
+                                color = {focused? '#007af3' : '#888'}
+                            />
+                        );
+                    },
+                    tabBarStyle: {
+                        backgroundColor: '#fff',
+                        borderTopColor: '#eee',
+                        height: 50,
+                    },
+                })}
+            >
+                <Tab.Screen name = "Home" component={Feed}/>
+                <Tab.Screen name = "AddPhoto" component={Feed}/>
+                <Tab.Screen name = "Profile" component={Feed}/>
+
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
+}
